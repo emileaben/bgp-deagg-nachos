f12ddc323376f67e52e30c03e69f34211d7ab257
Emile Aben <emileaben@gmail.com>
2016-11-21 10:31:57 +0100

    add rir to as2cc

--- fetch-as2cc.sh
+++ fetch-as2cc.sh
@@ -2,4 +2,4 @@ DATE=2016-11-01
 
 # BLOB: ripencc|RU|asn|206817|1|20161031|allocated
 
-ido +dc RIR_STATS +xT $DATE +R -M AS* +M +T | egrep BLOB | perl -F\\\| -lane'$end=$F[3]+$F[4]-1;$start=$F[3]; for ($start..$end) { print "$_\t$F[1]" }'
+ido +dc RIR_STATS +xT $DATE +R -M AS* +M +T | egrep BLOB | perl -F\\\| -lane'$end=$F[3]+$F[4]-1;$start=$F[3]; for ($start..$end) { $F[0] =~ s/^BLOB: // ; print "$_\t$F[1]\t$F[0]" }'
